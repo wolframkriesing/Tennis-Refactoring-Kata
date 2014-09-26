7cc86e7c73fdbb9b066658fe60f9c12fd57c196d
Wolfram Kriesing <wk@uxebu.com>
2014-09-26 13:00:08 +0200

    1h of refactoring with muenchner-singles.de, great progress!

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,67 +1,82 @@
-var TennisGame1 = function(player1Name, player2Name) {
-    this.m_score1 = 0;
-    this.m_score2 = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+var TennisGame1 = function() {
+  this.player1Score = 0;
+  this.player2Score = 0;
+  this.difference = 0;
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
-        this.m_score1 += 1;
-    else
-        this.m_score2 += 1;
+  var pointForPlayer1 = playerName === 'player1';
+  this.player1Score += pointForPlayer1 ? 1 : 0;
+  this.player2Score += pointForPlayer1 ? 0 : 1;
+  this.difference = this.player1Score - this.player2Score;
 };
 
 TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
-    }
-    return score;
+  if (this._isTie()) {
+    return this._getTieScoreString();
+  }
+  if (this.player1Score < 4 && this.player2Score < 4) {
+    return this._buildScoreString();
+  }
+  if (this._isAdvantage()) {
+    return this._getAdvantageScoreString();
+  }
+  return this._getWinScoreString();
+};
+
+
+
+
+TennisGame1.prototype._getTieScoreString = function() {
+  var ties = [
+    'Love-All',
+    'Fifteen-All',
+    'Thirty-All'
+  ];
+  if (this.player1Score < ties.length) {
+    return ties[this.player1Score];
+  }
+  return 'Deuce';
+};
+
+TennisGame1.prototype._getAdvantageScoreString = function() {
+  if (this.difference === 1) {
+    return "Advantage player1";
+  }
+  return "Advantage player2";
+};
+
+TennisGame1.prototype._getWinScoreString = function() {
+  if (this.difference >= 2) {
+    return "Win for player1";
+  }
+  return "Win for player2";
+};
+
+function _getScoreStringByPoints(score) {
+  var scoreToString = [
+    'Love',
+    'Fifteen',
+    'Thirty',
+    'Forty'
+  ];
+  return scoreToString[score];
+}
+
+TennisGame1.prototype._isTie = function() {
+  return this.difference === 0;
+};
+
+TennisGame1.prototype._isAdvantage = function() {
+  return Math.abs(this.difference) == 1;
+};
+TennisGame1.prototype._buildScoreString = function() {
+  return [
+    _getScoreStringByPoints(this.player1Score),
+    _getScoreStringByPoints(this.player2Score)
+  ].join('-');
 };
 
 if (typeof window === "undefined") {
-    module.exports = TennisGame1;
+  module.exports = TennisGame1;
 }
